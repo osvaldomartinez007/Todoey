d8154334adbbcb7a073181de678ad1fd2b852396
Osvaldo Martinez <omartinez@winona.edu>
2019-06-03 19:28:36 -0500

    convert to MVC design pattern

--- Todoey/Controller/TopdoListViewController.swift
+++ Todoey/Controller/TopdoListViewController.swift
@@ -17,20 +17,35 @@ class TodoListViewController : UITableViewController {
     // let cell : UITableViewCell
     
     // var textField = UITextField()   -  does not work here
-    var itemArray=["Find Nemo","Buy Eggs", "Destroy Demogorgon"]
+    //var itemArray=["Find Nemo","Buy Eggs", "Destroy Demogorgon"]
+    
+    var itemArray=[Item]()
     
     let defaults = UserDefaults.standard
-    
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let newItem = Item()
+        newItem.itemName = "Find Nemo"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.itemName = "Buy Eggs"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.itemName = "Destroy Demogorgon"
+        itemArray.append(newItem3)
+        
+        
         // problem here with itemArray is if the defaults does not exist ie the first time you run the program and then it will crash because no array has been copied into the p list defaults object
         
-        if let items = defaults.array(forKey: "ItemArray") as? [String] {
-            
+        if let items = defaults.array(forKey: "ItemArray") as? [Item] {
+
             itemArray=items
-             
+
         }
         
         print(itemArray)
@@ -52,32 +67,66 @@ class TodoListViewController : UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
 
+        let item = itemArray[indexPath.row]
         
-        cell.textLabel?.text=itemArray[indexPath.row]
+        
+        cell.textLabel?.text=item.itemName
+        
+        // cut down code with ternary operator
+        // value = condition ? valueIfTrue : valueIfFalse
+ 
+ cell.accessoryType = item.checked ? .checkmark : .none
+        
+// cell.accessoryType = item.checked == true ? .checkmark : .none
+        
+//                if item.checked == false {
+//
+//                    cell.accessoryType = .none
+//
+//                } else {
+//
+//                    cell.accessoryType = .checkmark
+//
+//                }
        
         return cell
         
     }
 
-// MARK - TableView Delegate Methods
+    //MARK: TableView Delegate Methods
+    
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
        
         // print(itemArray[indexPath.row])
+ 
         
-        tableView.deselectRow(at: indexPath, animated: true)
+        itemArray[indexPath.row].checked = !itemArray[indexPath.row].checked // sends back the opposite
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-            
-        } else {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-            
-        }
+//        if itemArray[indexPath.row].checked == false {
+//
+//            itemArray[indexPath.row].checked = true
+//
+//        } else {
+//
+//            itemArray[indexPath.row].checked = false
+//        }
+//
+         tableView.deselectRow(at: indexPath, animated: true)
+//
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+//
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//
+//        } else {
+//
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//
+//        }
+        
+        tableView.reloadData()
         
     }
     
@@ -101,7 +150,13 @@ let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: in
             print("success")
             print(textField.text) // this will print out as an optional and then inside will be the text
             print(textField.text!)
-            self.itemArray.append(textField.text!)
+            
+            let itemArrayToAdd = Item()
+            itemArrayToAdd.itemName=textField.text!
+            
+            
+            self.itemArray.append(itemArrayToAdd)
+            
             // can add a default value ?? "added item" for example. (textField.text ?? "added item") or you can put an if statement asking if its nil ""
             
             self.defaults.set(self.itemArray, forKey: "ItemArray") // in a closure so self for both items
@@ -116,6 +171,7 @@ let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: in
             textField = alertTextField
             print(alertTextField.text!)
             print(textField.text!)
+            print(self.itemArray)
             print("now")
         }
         
---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by OSVALDO MARTINEZ on 6/3/19.
+//  Copyright © 2019 OSVALDO MARTINEZ. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var itemName: String = ""
+    var checked: Bool = false
+    
+    
+}
